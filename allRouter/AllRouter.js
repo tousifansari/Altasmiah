940439da054a18e30b10bc89ccd031f072fe7bdd
Tosif Ansari <ansaritousif369@gmail.com>
2025-05-13 15:18:33 +0530

    Tumhara commit message yaha (e.g., Added new feature or Fixed bug)

---
+++ allRouter/AllRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.use("/api/Home", require("../Controllers/HomeController"));
+routes.use("/api/About", require("../Controllers/aboutController"));
+routes.use("/api/Blog", require("../Controllers/blogController"));
+routes.use("/api/GreenArcGallery", require("../Controllers/GalleryController"));
+routes.use("/api/UV_C_and_Blue_Light_Solutions", require("../Controllers/ServicesController"));
+routes.use("/api/sport_lighting", require("../Controllers/ServicesController"));
+routes.use("/api/Services_Commercial_Lighting", require("../Controllers/ServicesController"));
+routes.use("/api/TurnKey_Installation", require("../Controllers/ServicesController"));
+routes.use("/api/Energies", require("../Controllers/ServicesController"));
+routes.use("/api/Distribution", require("../Controllers/ServicesController"));
+routes.use("/api/Photometric_Analysis",require("../Controllers/ServicesController"));
+routes.use("/api/solutions", require("../Controllers/SolutionsController.js"));
+routes.use("/api/contact", require("../Controllers/contactController"));
+
+
+module.exports = routes;
