d2186e9ee25ca6ba321c4185240d03fec7d7e946
Tosif Ansari <ansaritousif369@gmail.com>
2025-05-15 12:45:51 +0530

    Add EJS files and fix server error

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -2,6 +2,6 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    res.render('Home', { title: 'Test Page' });
+    res.render('index', { title: 'Test Page' });
 });
 module.exports = routes;
